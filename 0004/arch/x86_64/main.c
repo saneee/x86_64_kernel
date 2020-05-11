8cc8c53ec71ac4c1e8dfc43cd40b83b79a20046d
saneee <399030@qq.com>
2020-05-11 14:45:02 +0800

    0004

---
+++ 0004/arch/x86_64/main.c
@@ -0,0 +1,16 @@
+#include <yaos/printk.h>
+#include <types.h>
+extern void print_regs();
+//the_cpu为每个cpu的数据，目前仅用于cpu栈
+char the_cpu[4096] __attribute__((section(".percpu")));
+void bp_main(void)
+{
+  char *w="Hello World!";
+  u64 *p = (u64*)0xb8000;
+  int i;
+  for(i=0;i<500;i++)*p++=0x0720072007200720;
+  printk("yaos printk:%s\n",w);
+  print_regs();
+  dump_mem((char*)0xc0000,0x40);
+  for(;;);
+}
